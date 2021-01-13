12d1ec243cb4d85d770c6e31697fb6bbe718285f
BreadBro <laughinsafin@gmail.com>
2021-01-13 10:48:12 -0600

    Java HW 1

---
+++ Java HW 1.java
@@ -0,0 +1,49 @@
+
+class JavaHW {
+/*
+6. println, sum_of_data, _average, AnnualSalary, B4, ABC
+
+8. name    age     height
+   Archie  17      5'9"
+   Betty   17      5'6"
+   Jughead 16      6'
+
+10.         a       b       c
+\\
+'
+"""
+C:
+in      he downward spiral
+
+12. 
+System.out.println("\"Several slashes are sometimes seen,\"");
+System.out.println("said Sally. \"I've said so.\" See?");
+System.out.println("\\ / \\\\ // \\\\\\ ///");
+
+14. System.out.println("/ \\ // \\\\ /// \\\\\\");
+
+16. syntax error on public MyProgram
+semicolon at end of 3rd line
+println capitalized on line 4
+
+18. comments out line 9,10
+parentheses needed on line 11
+FormalParameterList is uncompleted
+main cant be declared static
+*/
+/* Exercise 7 */
+    public static void main(String args[]) 
+    {
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
