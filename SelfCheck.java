3d481bc948d11852f8ece69e091f5a42de1ef8e7
BreadBro <48104087+BreadBro@users.noreply.github.com>
2021-01-25 14:20:27 -0600

    Add files via upload

---
+++ SelfCheck.java
@@ -0,0 +1,107 @@
+public class SelfCheck {
+/*
+2a. 8
+2b. 11
+2c. 6
+2d. 4
+2e. 33
+2f. -16
+2g. 6.4
+2h. 6
+2i. 30
+2j. 1
+2k. 7
+2l. 5
+2m. 2
+2n. 18
+2o. 3
+2p. 4
+2q. 4
+2r. 15
+2s. 8
+2t. 1
+
+4a. 11
+4b. 236
+4c. 56
+4d. 74
+4e. 29
+4f. 21
+4g. hello 348
+4h. 2(int) 2.042
+4i. 14.73
+4j. 007 (James Bond)
+4k. -2583
+4l. 644
+4m. 123430717
+
+6. 
+    public static void Year() {
+        String Year;
+        Year = "Sophmore";
+        System.out.println(Year);
+    }
+    public static void GPA() {
+        double GPA = 3.6;
+        System.out.println(GPA);
+
+8. % 10, %100
+
+10. 
+first += second
+-second -= first
+first -= second
+
+12. 
+26
+36
+23
+13
+
+14a. count*2
+14b. 4+15*count
+14c. -10*count+40
+14d. count*4-11
+14e. count*-3+100
+
+16. 
+4
+2
+
+18. 
++---+
+\  /
+/  \
+\  /
+/  \
+\  /
+/  \
++---+
+
+20. T-minus 5, 4, 3, 2, 1, Blastoff!
+
+22. 
+         *
+        ***
+       *****
+      *******
+     *********
+    ***********
+   *************
+  ***************
+ *****************
+*******************
+
+24. 
+************!
+************!
+
+26. changes a final in the code which is impossible, count can't be used outside the loop, needs to be count + 1 instead of count + 2, change MaxOdd from final
+
+28a. (line+SIZE)*2
+28b. 4*line+SIZE*3
+28c. -line + (SIZE*2 - 3)
+
+30. I don't really understand what this question is asking.
+*/
+}
